b08571b26330cd7bace8d9b5e3b08efdbc199502
Hyuein <102200716+Hyuein@users.noreply.github.com>
2022-04-20 22:46:31 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,180 @@
+var config = {
+    style: 'mapbox://styles/ead2200/cl1ee21g6001r14mt3c01jl56',
+    accessToken: 'pk.eyJ1IjoiZWFkMjIwMCIsImEiOiJja3M2ZTIzcmIwOTg5MndwNXM5ODZ5d2J3In0.LQXO-PEblZkmvFSlZpSS1w',
+    showMarkers: false,
+    theme: 'light',
+    title: '',
+    subtitle: '',
+    byline: '',
+    footer: '',
+    chapters: [
+        {
+            id: 'chapter1',
+            alignment: 'right',
+            title: 'MINING',
+            image: '',
+            description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
+            location: {
+                center: [-74.076,42.793],
+                zoom: 7,
+                pitch: 0,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {layer: 'Tourism copy', opacity: 0 },
+                {layer: 'Tourism', opacity: 0 },
+                {layer: 'Shipping and Trade copy', opacity:0 },
+                {layer: 'Shipping and Trade', opacity:0 },
+                {layer: 'Agriculture copy', opacity:0 },
+                {layer: 'Agriculture', opacity:0 },
+                {layer: 'Manufacturing copy', opacity:0 },
+                {layer: 'Manufacturing', opacity:0 },
+                {layer: 'Mining copy', opacity:0 },
+                {layer: 'Mining', opacity:1 }
+            ],
+            onChapterExit: [
+              
+            ]
+        },
+        {
+            id: 'chapter2',
+            alignment: 'right',
+            title: 'MANUFACTURING',
+            image: '',
+            description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
+            location: {
+                center: [-74.076,42.793],
+                zoom: 7,
+                pitch: 0,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {layer: 'Tourism copy', opacity: 0 },
+                {layer: 'Tourism', opacity: 0 },
+                {layer: 'Shipping and Trade copy', opacity:0 },
+                {layer: 'Shipping and Trade', opacity:0 },
+                {layer: 'Agriculture copy', opacity:0 },
+                {layer: 'Agriculture', opacity:0 },
+                {layer: 'Manufacturing copy', opacity:0 },
+                {layer: 'Manufacturing', opacity:1 },
+                {layer: 'Mining copy', opacity:1 },
+                {layer: 'Mining', opacity:0 }
+            ],
+            onChapterExit: [
+              
+            ]
+        },
+        {
+            id: 'chapter3',
+            alignment: 'right',
+            title: 'AGRICULTURE',
+            image: '',
+            description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
+            location: {
+                center: [-74.076,42.793],
+                zoom: 7,
+                pitch: 0,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {layer: 'Tourism copy', opacity: 0 },
+                {layer: 'Tourism', opacity: 0 },
+                {layer: 'Shipping and Trade copy', opacity:0 },
+                {layer: 'Shipping and Trade', opacity:0 },
+                {layer: 'Agriculture copy', opacity:0 },
+                {layer: 'Agriculture', opacity:1 },
+                {layer: 'Manufacturing copy', opacity:1 },
+                {layer: 'Manufacturing', opacity:0 },
+                {layer: 'Mining copy', opacity:1 },
+                {layer: 'Mining', opacity:0 }
+            ],
+            onChapterExit: [
+              
+            ]
+        },
+        {
+            id: 'chapter4',
+            alignment: 'right',
+            title: 'SHIPPING & TRADE',
+            image: '',
+            description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
+            location: {
+                center: [-74.076,42.793],
+                zoom: 7,
+                pitch: 0,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {layer: 'Tourism copy', opacity: 0 },
+                {layer: 'Tourism', opacity: 0 },
+                {layer: 'Shipping and Trade copy', opacity:0 },
+                {layer: 'Shipping and Trade', opacity:1 },
+                {layer: 'Agriculture copy', opacity:1 },
+                {layer: 'Agriculture', opacity:0 },
+                {layer: 'Manufacturing copy', opacity:1 },
+                {layer: 'Manufacturing', opacity:0 },
+                {layer: 'Mining copy', opacity:1 },
+                {layer: 'Mining', opacity:0 }
+            ],
+            onChapterExit: [
+              
+            ]
+        },
+        {
+            id: 'chapter5',
+            alignment: 'right',
+            title: 'MINING',
+            image: '',
+            description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
+            location: {
+                center: [-74.076,42.793],
+                zoom: 7,
+                pitch: 0,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {layer: 'Tourism copy', opacity: 0 },
+                {layer: 'Tourism', opacity: 1 },
+                {layer: 'Shipping and Trade copy', opacity:1 },
+                {layer: 'Shipping and Trade', opacity:0 },
+                {layer: 'Agriculture copy', opacity:1 },
+                {layer: 'Agriculture', opacity:0 },
+                {layer: 'Manufacturing copy', opacity:1 },
+                {layer: 'Manufacturing', opacity:0 },
+                {layer: 'Mining copy', opacity:1 },
+                {layer: 'Mining', opacity:0 }
+            ],
+            onChapterExit: [
+              
+            ]
+        },
+        {
+            id: 'chapter6',
+            alignment: 'right',
+            title: 'ALL INDUSTRIES',
+            image: '',
+            description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
+            location: {
+                center: [-74.076,42.793],
+                zoom: 7,
+                pitch: 0,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {layer: 'Tourism copy', opacity: 0 },
+                {layer: 'Tourism', opacity: 1 },
+                {layer: 'Shipping and Trade copy', opacity:0 },
+                {layer: 'Shipping and Trade', opacity:1 },
+                {layer: 'Agriculture copy', opacity:0 },
+                {layer: 'Agriculture', opacity:1 },
+                {layer: 'Manufacturing copy', opacity:0 },
+                {layer: 'Manufacturing', opacity:1 },
+                {layer: 'Mining copy', opacity:0 },
+                {layer: 'Mining', opacity:1 }
+            ],
+            onChapterExit: [
+              
+            ]
+        }
+    ]
+};
